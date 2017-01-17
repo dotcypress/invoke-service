2cf68e6d5c0a75fbf9502663f8c3f28a6a2552f6
Vitaly Domnikov <dotcypress@gmail.com>
2017-01-16 18:41:30 -0800

    switch to isomofphic fetch

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const fetch = require('node-fetch')
-const querystring = require('querystring')
+const fetch = require('isomorphic-fetch')
 
 module.exports = function (endpoint, opts) {
   const options = Object.assign({
@@ -10,7 +9,7 @@ module.exports = function (endpoint, opts) {
     options.headers.authorization = `Bearer ${options.token}`
   }
   return options.method === 'GET'
-    ? (params = {}) => fetch(`${endpoint}?${querystring.stringify(params)}`, options).then(unwrap)
+    ? (params = {}) => fetch(`${endpoint}?${queryString(params)}`, options).then(unwrap)
     : (params = {}) => {
       const fetchOpts = Object.assign({
         body: JSON.stringify(params)
@@ -20,17 +19,22 @@ module.exports = function (endpoint, opts) {
     }
 }
 
+function queryString (obj = {}) {
+  return Object.keys(obj)
+    .map((key) => `${key}=${encodeURIComponent(obj[key])}`)
+    .join('&')
+}
+
 function unwrap (res) {
   if (!res.ok) {
     const err = new Error(res.statusText || 'Error calling service')
     err.statusCode = res.status
     throw err
   }
-  return res.json()
-    .then((json) => {
-      if (('ok' in json && !json.ok) || ('status' in json && json.status !== 'success')) {
-        throw new Error(json.error || json.message || 'Error calling service')
-      }
-      return json.data
-    })
+  return res.json().then((json) => {
+    if (('ok' in json && !json.ok) || ('status' in json && json.status !== 'success')) {
+      throw new Error(json.error || json.message || 'Error calling service')
+    }
+    return json.data
+  })
 }
